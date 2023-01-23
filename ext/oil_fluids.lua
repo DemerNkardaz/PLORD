9f95aa9f9ae37a9d1ef94e46db0a494a758949dd
DemerNkardaz <92405785+DemerNkardaz@users.noreply.github.com>
2023-01-23 19:26:35 +0200

    Create oil_fluids.lua

---
+++ ext/oil_fluids.lua
@@ -0,0 +1,14 @@
+data:extend({
+	{
+	type = "fluid",
+	name = "PLORD_oil_gasoline",
+	default_temperature = 17,
+	max_temperature = 50,
+	heat_capacity = "1.4KJ",
+	base_color = {r=0.9, g=0.9, b=0.8},
+	flow_color = {r=0.9, g=0.9, b=0.8},
+	icon = "__PLORD__/graphics/icons/fluid/oil-gasoline.png",
+	icon_size = 64, icon_mipmaps = 4,
+	order = "b[fluid]-a[oil-gasoline]-aa"
+	},
+})
